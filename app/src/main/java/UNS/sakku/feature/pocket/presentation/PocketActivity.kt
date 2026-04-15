e8e557656ef1f757ed4ad20388cac301d8677669
Master-FroSt <mfschannel30@gmail.com>
2026-04-15 15:34:24 +0700

    Pindah MainActivity.kt ke LoginActivity.kt

--- app/src/main/java/UNS/sakku/feature/pocket/presentation/PocketActivity.kt
+++ app/src/main/java/UNS/sakku/feature/pocket/presentation/PocketActivity.kt
@@ -3,7 +3,7 @@ package uns.sakku.feature.pocket.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-class NotificationActivity : ComponentActivity() {
+class PocketActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
