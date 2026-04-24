088673c4aed19e2b47a7ab3ef5e3c6f338ea7cd5
Master-FroSt <mfschannel30@gmail.com>
2026-04-24 18:25:25 +0700

    Mini Bug di DashboardScreen.kt masuk Login

--- app/src/main/java/UNS/sakku/feature/pocket/presentation/AddPocketSavingScreen.kt
+++ app/src/main/java/UNS/sakku/feature/pocket/presentation/AddPocketSavingScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
